b7918482efea10c1cf0ef416c7297319bfa80adf
preeti <parumalla385@gmail.com>
2020-08-06 02:54:01 -0400

    Intial commit

---
+++ vue-portfolio/src/components/navigation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+              data-target="#navbarTogglerDemo03"
+              aria-controls="navbarTogglerDemo03"
+              aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Preeti Arumalla</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+                    <router-link to="/">Home</router-link>
+                    <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                    <router-link to="/about">About</router-link>
+                    <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item" @click="jumpToPortfolio" v-scroll-to="'#portf'" >
+                    <a class="nav-link" href="#">Portfolio</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+import Home from '../views/Home'
+import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
+
+Vue.use(VueScrollTo)
+
+export default {
+  methods: {
+    jumpToPortfolio () {
+      if (this.$route.path === '/about') {
+        document.getElementById('portf').scrollIntoView()
+      }
+    }
+  }
+}
+</script>
---
+++ vue-portfolio/src/views/Home.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <section class="section">
+            <div class="container-fliud">
+                <img alt="Vue logo" class="rounded-circle w-25 h-25" src="../assets/DSC_1425 copy.jpeg">
+                <h1>Software Developer</h1>
+                <p>This is a fun project I built while learning
+                   vue.js and bootstrap
+                </p>
+                <button type="button" class="btn btn-primary btn-lg">
+                  Contact
+                </button>
+            </div>
+        </section>
+        <section class="portfolio"  id="portf">
+            <h1>Portfolio</h1>
+            <p>These are suppose to be sample projects</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo"
+                                      class="rounded-circle img-custome"
+                                      src="https://www.wetechnology.cz/wp-content/uploads/2017/02/uxui.jpg">
+                                    <h2>web project</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Donec sed odio dui.
+                                        Etiam porta sem malesuada magna mollis euismod.
+                                        Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                        Morbi leo risus, porta ac consectetur ac,
+                                        vestibulum at eros. Praesent commodo cursus magna.
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">
+                                      More
+                                    </button>
+                                </div>
+                            </div>
+                            <br>
+                    </div>
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo"
+                                        class="rounded-circle img-custome"
+                                        src="https://i.pinimg.com/originals/4a/70/56/4a70563234301578c158dc142384a1c3.png">
+                                    <h2>web project</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
+                                        Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">More</button>
+                                </div>
+                            </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-head">
+                                <img alt="Vue logo"
+                                    class="rounded-circle img-custome"
+                                    src="https://cdn.mos.cms.futurecdn.net/wH5y2PkWhCxqKBje8rBp5e.jpg">
+                                <h2>web project</h2>
+                            </div>
+                            <hr>
+                            <div class="card-body">
+                                <p>
+                                    Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
+                                    Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                </p>
+                                <button type="button" class="btn btn-outline-secondary">More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'Home'
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+    .section{
+        padding-top: 10%;
+        padding-bottom: 10%;
+    }
+    .portfolio{
+        background-color: #34345238;
+        padding-top: 8%;
+        padding-bottom: 15%;
+        /* padding:15% */
+    }
+    .img-custome{
+        width: 140px;
+        height: 140px;
+        margin-top: 15px;
+    }
+</style>
